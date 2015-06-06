f713ebb1656c0bc6bf7d5b5180c48167e270981f
fBrx <admin@florian-m.net>
2015-06-06 15:31:27 +0200

    added loop with timer

--- noipupdate.go
+++ noipupdate.go
@@ -3,21 +3,39 @@ package main
 import (
     "fmt"
     "net"
+    "log"
+    "time"
 )
 
 func main() {
-    resolveIp("fbrx.noip.me")
+    var ip, newIp string
+    for{
+        newIp = resolveIp("fbrx.noip.me")
+        fmt.Printf("current ip: %v - ip from dns: %v\n", ip, newIp)
+        if(ip == newIp){
+            fmt.Println("no change detected...")
+        }else{
+            fmt.Println("deteced change...triggering update")
+            ip = updateIp(newIp)
+        }
+        time.Sleep(2 * time.Second)
+    }
 }
 
 
 func resolveIp(host string) string {
     var addrs, err = net.LookupHost(host)
     fmt.Println("looking up host: " + host)
-    if(err == nil){
-        fmt.Println("resolved to " + addrs[0])
-        return addrs[0]
-    }else{
-        fmt.Println(err)
-        return "";
+    if(err != nil){
+        log.Fatal(err)
     }
+
+    fmt.Println("resolved to " + addrs[0])
+    return addrs[0]
+
+}
+
+func updateIp(newIp string) string {
+    fmt.Println("...updated...")
+    return newIp
 }
