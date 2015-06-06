e188e6c9e3af72b9fb8f4851b086cb1772471783
fBrx <admin@florian-m.net>
2015-06-06 16:07:47 +0200

    added current ip lookup

--- noipupdate.go
+++ noipupdate.go
@@ -3,30 +3,35 @@ package main
 import (
     "fmt"
     "net"
+    "net/http"
     "log"
     "time"
+    "io/ioutil"
 )
 
+var waitTime int = 10
+var host string = "fbrx.noip.me"
+var ipResolverHost string = "http://whatismyip.akamai.com/" //http://v4.ident.me/
+
 func main() {
-    var ip, newIp string
-    var waitTime int = 10
-    var host string = "fbrx.noip.me"
+    var currentIp, dnsIp string
     for{
         fmt.Println(time.Now())
-        newIp = resolveIp(host)
-        fmt.Printf("current ip: %v - ip from dns: %v\n", ip, newIp)
-        if(ip == newIp){
+        currentIp = determineCurrentIp()
+        dnsIp = resolveIpFromDns(host)
+        fmt.Printf("current ip: %v - ip from dns: %v\n", currentIp, dnsIp)
+        if(currentIp == dnsIp){
             fmt.Println("no change detected...")
         }else{
             fmt.Println("deteced change...triggering update")
-            ip = updateIp(newIp)
+            updateIp(currentIp)
         }
         time.Sleep((time.Duration(waitTime) * time.Second))
     }
 }
 
 
-func resolveIp(host string) string {
+func resolveIpFromDns(host string) string {
     var addrs, err = net.LookupHost(host)
     fmt.Println("looking up host: " + host)
     if(err != nil){
@@ -35,10 +40,25 @@ func resolveIp(host string) string {
 
     fmt.Println("resolved to " + addrs[0])
     return addrs[0]
+}
 
+func determineCurrentIp() string {
+    resp, err := http.Get(ipResolverHost)
+    if(err != nil){
+        log.Fatal(err)
+    }
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if(err != nil){
+        log.Fatal(err)
+    }
+    fmt.Println("determined current ip: " + string(body))
+
+    return string(body)
 }
 
 func updateIp(newIp string) string {
-    fmt.Println("...updated...")
+    fmt.Printf("...updated to %v...\n")
     return newIp
 }
